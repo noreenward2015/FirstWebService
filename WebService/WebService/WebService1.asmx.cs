8035992e789664e7a69dca29cc6c8fc3e444d4d0
starlord2015 <noreen.ward@rocketmail.com>
2015-02-20 20:49:17 +0000

    Add and multiply numbers

---
+++ WebService/WebService/WebService1.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://noreenFirstWebService.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+     
+    [WebMethod(Description = "add two numbers")]
+        public int add(int nbr1, int nbr2)
+        {
+            return nbr1 + nbr2;
+        }
+
+    [WebMethod(Description = "multiply two numbers")]
+    public int mul(int nbr1, int nbr2)
+    {
+        return nbr1 * nbr2;
+    }
+    
+    
+    }
+}
---
+++ WebService/consumeWS/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using consumeWS.NoreenWS;
+
+namespace consumeWS
+{
+    public partial class Form1 : Form
+    {
+        private WebService1SoapClient ws = new WebService1SoapClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ws.add((int)numericUpDown1.Value, (int)numericUpDown2.Value).ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = ws.add((int)numericUpDown1.Value, (int)numericUpDown2.Value).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ws.mul((int)numericUpDown1.Value, (int)numericUpDown2.Value).ToString();
+        }
+
+ 
+
+    }
+
+}
